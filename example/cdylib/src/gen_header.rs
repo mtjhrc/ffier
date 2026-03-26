66d51b51532a58c21db8ae612673e341017efc38
Matej Hrica <mhrica@redhat.com>
2026-03-26 19:28:44 +0100

    Restructure generated C headers around HeaderBuilder
    Return structured header sections from the exportable, implementable, and FfiError macros so cdylibs can assemble a single deduplicated header. Also treat builder-style Self returns as in-place handle updates and re-export paste for generated error bridge helpers.
    
    Signed-off-by: Matej Hrica <mhrica@redhat.com>

--- example/cdylib/src/gen_header.rs
+++ example/cdylib/src/gen_header.rs
@@ -5,8 +5,9 @@ example_lib::mycalculator_ffier!(MyCalculator);
 example_lib::calcresult_ffier!(CalcResult);
 
 fn main() {
-    // CalcResult header first since MyCalculator references it
-    print!("{}", ex_calcresult__header());
-    println!();
-    print!("{}", ex_mycalculator__header());
+    let header = ffier::HeaderBuilder::new("EX_H")
+        .add(ex_calcresult__header())
+        .add(ex_mycalculator__header())
+        .build();
+    print!("{header}");
 }
